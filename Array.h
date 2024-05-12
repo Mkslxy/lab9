76aff28d32b686139eee060c6934cacb56e67271
PEtyrchachny Alexey <leshalol66@gmail.com>
2024-05-12 19:09:01 +0300

    shiftElement

--- Array.h
+++ Array.h
@@ -1,14 +1,1 @@
-//
-// Created by lesha on 12.05.2024.
-//
 
-#ifndef LAB9_ARRAY_H
-#define LAB9_ARRAY_H
-
-
-class Array {
-
-};
-
-
-#endif //LAB9_ARRAY_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
-int main() {
+template<typename T, size_t H>
+void shiftElement(int counter, array<T, H> &arr) {
+    array<T, H> test;
+    for (int i = 0; i < H; ++i) {
+        test[(i + counter + H) % H] = arr[i];
+    }
+    arr = test;
+};
 
+int main() {
+    array<int, 5> test = {1, 2, 3, 4, 5};
+    shiftElement(4, test);
+    for (const auto &element : test) {
+        cout << element << " ";
+    }
+    cout << endl;
+    return 0;
 }
